5be89562be171d9c4f2585f9372ed122cc259409
Pietro-H <pietro.henrique.pereira@escola.pr.gov.br>
2025-07-31 12:18:31 +0000

    fiz o banner do meu jogo.

---
+++ jogo.py
@@ -0,0 +1,13 @@
+import random
+
+print("{{{{{{{{{{{{{{{{!}}}}}}}}}}}}}}}}")
+print("{{{                           }}}")
+print("{{     JOGO DE ADIVINHAÇÃO     }}")
+print("{              DO               }")
+print("{{       Pietro Henrique       }}")
+print("{{{                           }}}")
+print("{{{{{{{{{{{{{{{{!}}}}}}}}}}}}}}}}")
+
+numeroSecreto= random.radrange(0,100)
+totalTentativas = 0 
+pontos = 100
